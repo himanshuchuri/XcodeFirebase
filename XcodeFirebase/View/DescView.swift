c7151a9a5afb2992e690b31001f6d4165c0c5262
Himanshu Churi <himanshuchuri@gmail.com>
2022-01-13 20:13:23 -0500

    Created Required Blank Files

---
+++ XcodeFirebase/View/DescView.swift
@@ -0,0 +1,20 @@
+//
+//  DescView.swift
+//  XcodeFirebase
+//
+//  Created by Himanshu Churi on 1/13/22.
+//
+
+import SwiftUI
+
+struct DescView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DescView_Previews: PreviewProvider {
+    static var previews: some View {
+        DescView()
+    }
+}
---
+++ XcodeFirebase/View/MergeView.swift
@@ -0,0 +1,20 @@
+//
+//  MergeView.swift
+//  XcodeFirebase
+//
+//  Created by Himanshu Churi on 1/13/22.
+//
+
+import SwiftUI
+
+struct MergeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MergeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MergeView()
+    }
+}
