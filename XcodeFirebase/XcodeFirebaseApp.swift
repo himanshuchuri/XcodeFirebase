22fd8702669b0ace20268cc7f8a969b4260061b0
Himanshu Churi <himanshuchuri@gmail.com>
2022-01-13 19:57:57 -0500

    Initial Commit

---
+++ XcodeFirebase/XcodeFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeFirebaseApp.swift
+//  XcodeFirebase
+//
+//  Created by Himanshu Churi on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct XcodeFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
